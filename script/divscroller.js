4b4a50bcf8473f28da42cd510111d400dcd89fec
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2023-06-13 17:49:41 +0200

    add styles

--- script/divscroller.js
+++ script/divscroller.js
@@ -1,7 +1,7 @@
 function scrollToDiv(className) {
     const element = document.querySelector(className);
     if (element) {
-        const elementPosition = element.offsetTop - 60; // Pozycja diva minus 60px
+        const elementPosition = element.offsetTop - 40; // Pozycja diva minus 60px
         window.scrollTo({top: elementPosition, behavior: 'smooth'});
     }
 }
